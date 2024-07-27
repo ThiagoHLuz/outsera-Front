4d8d5cec51958ed7dc70860a6fe5886d9996d8b1
Thiago Henrique Luz <79991511+ThiagoHLuz@users.noreply.github.com>
2024-07-27 13:14:30 -0300

    Correção responsividade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,9 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app.routes';
 import { NgxPaginationModule } from 'ngx-pagination';
-import {AppComponent} from "./app.component";
-import {CommonModule} from "@angular/common";
+import { AppComponent} from "./app.component";
+import { CommonModule} from "@angular/common";
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 
@@ -21,7 +22,8 @@ import {CommonModule} from "@angular/common";
     FormsModule,
     NgxPaginationModule,
     BrowserAnimationsModule,
-    CommonModule
+    CommonModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
